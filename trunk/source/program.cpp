88d93e9886cdaa5550e72b8e459fabc4b09e12e2
ryoohki <ryoohki@dc685512-7b07-11de-a624-ab7c8f4ec817>
2009-07-30 22:28:45 +0000

    Adjusted output to use consistent wide characters. Oopsies.

--- trunk/source/program.cpp
+++ trunk/source/program.cpp
@@ -20,14 +20,14 @@ int wmain(int argc, wchar_t** argv) {
 	fout<<L"<?xml version=\"1.0\" encoding=\"utf-8\"?>"<<std::endl;
 	fout<<L"<methods>"<<std::endl;
 	for(std::size_t i = 0; i < info.second.size(); ++i) {
-		fout<<"    <method name='"<<info.second[i].MethodName<<"'>"<<std::endl;
+		fout<<L"    <method name='"<<info.second[i].MethodName<<L"'>"<<std::endl;
 		for(std::size_t j = 0; j < info.second[i].CodeChunks.size(); ++j) {
-			fout<<"        <rva length='"
+			fout<<L"        <rva length='"
 				<<info.second[i].CodeChunks[j].length
-				<<"'>"
-				<<info.second[i].CodeChunks[j].startAddr - info.second[i].BaseAddress<<"</rva>"<<std::endl;
+				<<L"'>"
+				<<info.second[i].CodeChunks[j].startAddr - info.second[i].BaseAddress<<L"</rva>"<<std::endl;
 		}
-		fout<<"    </method>"<<std::endl;
+		fout<<L"    </method>"<<std::endl;
 	}
 	fout<<L"</methods>"<<std::endl;
 }
