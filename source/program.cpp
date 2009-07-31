3f56598a8e1eead04a6bd3b5f6b477b72176e609
ryoohki <ryoohki@dc685512-7b07-11de-a624-ab7c8f4ec817>
2009-07-31 06:27:13 +0000

    Now with enhanced x64 support. And added license header to all files that matter.

--- source/program.cpp
+++ source/program.cpp
@@ -1,3 +1,25 @@
+/*
+* Copyright (c) 2007-2009 SlimGen Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
 #include "stdafx.h"
 #include "Debugger\Debugger.h"
 
@@ -26,6 +48,12 @@ void GenerateTemplateAsm(std::wstring methodName, CORDB_ADDRESS rva, std::size_t
 		<<L";       stack right to left."<<std::endl
 #endif
 		<<L";-------------------------------------------------------------------------------"<<std::endl
+#ifdef _M_X64
+		<<L"BITS        64"<<std::endl
+#else
+		<<L"BITS        32"<<std::endl
+#endif
+		<<L"ORG         0x"<<std::hex<<rva<<std::endl
 		<<L"start:"<<std::endl
 		<<L"            ret      4"<<std::endl
 		<<L";-------------------------------------------------------------------------------"<<std::endl
