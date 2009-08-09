07c6358b7d930516f7f99f4a236e1d4dc564d438
ryoohki <ryoohki@dc685512-7b07-11de-a624-ab7c8f4ec817>
2009-08-09 09:18:07 +0000

    Now with 100% more error checking! All natural fruit juice too!
    Refactored out some methods in the injection code to help clean it up.
    
    Wrapped host process in a try/catch to eliminate issues with assembly not being found.

--- HostProcess/Program.cs
+++ HostProcess/Program.cs
@@ -24,30 +24,28 @@ using System;
 using System.IO;
 using System.Reflection;
 
-namespace HostProcess
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length != 1)
-            {
+namespace HostProcess {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length != 1) {
                 Console.WriteLine("Usage: HostProcess.exe <AssemblyName>");
                 return;
             }
 
-            var fi = new FileInfo(args[0]);
+            try {
+                var fi = new FileInfo(args[0]);
+                var assembly = fi.Exists
+                                   ? Assembly.LoadFile(fi.FullName)
+                                   : AppDomain.CurrentDomain.Load(new AssemblyName(args[0]));
 
-            var assembly = fi.Exists ? Assembly.LoadFile(fi.FullName) : AppDomain.CurrentDomain.Load(new AssemblyName(args[0]));
+                foreach (var type in assembly.GetTypes()) {
+                    type.GetMethods();
+                }
 
-            foreach (var type in assembly.GetTypes())
-            {
-                type.GetMethods();
-            }
-
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                System.Diagnostics.Debugger.Break();
+                if (System.Diagnostics.Debugger.IsAttached) {
+                    System.Diagnostics.Debugger.Break();
+                }
+            } catch {
             }
         }
     }
