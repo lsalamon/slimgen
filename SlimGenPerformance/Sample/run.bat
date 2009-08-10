677e06998b904e5f157e4509ad3d2c130e17d3a8
ryoohki <ryoohki@dc685512-7b07-11de-a624-ab7c8f4ec817>
2009-08-10 05:45:18 +0000

    Modified run.bat to not require VS... I think.

--- SlimGenPerformance/Sample/run.bat
+++ SlimGenPerformance/Sample/run.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 ECHO ================================================================================This requires administrative permissions to run!
 ECHO ================================================================================Working...
-NGEN uninstall /silent SlimGen.Performance.Test.exe
-NGEN uninstall /silent SlimGen.Performance.Math.dll
-NGEN install /silent SlimGen.Performance.Math.dll
-NGEN install /silent SlimGen.Performance.Test.exe
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\ngen.exe uninstall /silent SlimGen.Performance.Test.exe
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\ngen.exe uninstall /silent SlimGen.Performance.Math.dll
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\ngen.exe install /silent SlimGen.Performance.Math.dll
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\ngen.exe install /silent SlimGen.Performance.Test.exe
 SlimGen.Compiler test.xml test.sgen
 SlimGen.Client SlimGen.Performance.Math.dll test.sgen > NUL
 ECHO ================================================================================Beggining test run
