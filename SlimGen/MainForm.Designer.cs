50a8e7f26a075ecb0566089cdacf3ba1ca276347
Mike.Popoloski <Mike.Popoloski@dc685512-7b07-11de-a624-ab7c8f4ec817>
2009-08-13 15:24:03 +0000

    Implemented the Generate Templates feature in the GUI.

--- SlimGen/MainForm.Designer.cs
+++ SlimGen/MainForm.Designer.cs
@@ -47,7 +47,7 @@
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox.Controls.Add(this.tableLayoutPanel);
             this.groupBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.groupBox.Location = new System.Drawing.Point(11, 11);
+            this.groupBox.Location = new System.Drawing.Point(10, 12);
             this.groupBox.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox.Name = "groupBox";
             this.groupBox.Padding = new System.Windows.Forms.Padding(2);
@@ -68,96 +68,97 @@
             this.tableLayoutPanel.Controls.Add(this.ngenButton, 0, 2);
             this.tableLayoutPanel.Controls.Add(this.createXmlButton, 1, 0);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel.Location = new System.Drawing.Point(2, 22);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(2, 18);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 3;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33555F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33223F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33223F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(527, 333);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(527, 337);
             this.tableLayoutPanel.TabIndex = 6;
             // 
             // templatesButton
             // 
             this.templatesButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.templatesButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.templatesButton.Image = global::SlimGen.Properties.Resources.templates;
             this.templatesButton.Location = new System.Drawing.Point(3, 3);
             this.templatesButton.Name = "templatesButton";
-            this.templatesButton.Size = new System.Drawing.Size(257, 105);
+            this.templatesButton.Size = new System.Drawing.Size(257, 106);
             this.templatesButton.TabIndex = 0;
             this.templatesButton.Text = "Generate Templates";
+            this.templatesButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.templatesButton.UseVisualStyleBackColor = true;
             this.templatesButton.Click += new System.EventHandler(this.templatesButton_Click);
             // 
             // testButton
             // 
             this.testButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.testButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.testButton.Location = new System.Drawing.Point(266, 224);
+            this.testButton.Image = global::SlimGen.Properties.Resources.test;
+            this.testButton.Location = new System.Drawing.Point(266, 227);
             this.testButton.Name = "testButton";
-            this.testButton.Size = new System.Drawing.Size(258, 106);
+            this.testButton.Size = new System.Drawing.Size(258, 107);
             this.testButton.TabIndex = 4;
             this.testButton.Text = "Performance Test";
+            this.testButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.testButton.UseVisualStyleBackColor = true;
-            this.testButton.Click += new System.EventHandler(this.testButton_Click);
             // 
             // editXmlButton
             // 
             this.editXmlButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.editXmlButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.editXmlButton.Location = new System.Drawing.Point(266, 114);
+            this.editXmlButton.Image = global::SlimGen.Properties.Resources.edit;
+            this.editXmlButton.Location = new System.Drawing.Point(266, 115);
             this.editXmlButton.Name = "editXmlButton";
-            this.editXmlButton.Size = new System.Drawing.Size(258, 104);
+            this.editXmlButton.Size = new System.Drawing.Size(258, 106);
             this.editXmlButton.TabIndex = 5;
             this.editXmlButton.Text = "Edit XML Configuration";
+            this.editXmlButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.editXmlButton.UseVisualStyleBackColor = true;
-            this.editXmlButton.Click += new System.EventHandler(this.editXmlButton_Click);
             // 
             // compileButton
             // 
             this.compileButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.compileButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.compileButton.Location = new System.Drawing.Point(3, 114);
+            this.compileButton.Image = global::SlimGen.Properties.Resources.compile;
+            this.compileButton.Location = new System.Drawing.Point(3, 115);
             this.compileButton.Name = "compileButton";
-            this.compileButton.Size = new System.Drawing.Size(257, 104);
+            this.compileButton.Size = new System.Drawing.Size(257, 106);
             this.compileButton.TabIndex = 1;
             this.compileButton.Text = "Compile To SGen";
+            this.compileButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.compileButton.UseVisualStyleBackColor = true;
-            this.compileButton.Click += new System.EventHandler(this.compileButton_Click);
             // 
             // ngenButton
             // 
             this.ngenButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ngenButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.ngenButton.Location = new System.Drawing.Point(3, 224);
+            this.ngenButton.Image = global::SlimGen.Properties.Resources.ngen;
+            this.ngenButton.Location = new System.Drawing.Point(3, 227);
             this.ngenButton.Name = "ngenButton";
-            this.ngenButton.Size = new System.Drawing.Size(257, 106);
+            this.ngenButton.Size = new System.Drawing.Size(257, 107);
             this.ngenButton.TabIndex = 2;
             this.ngenButton.Text = "NGEN an Assembly";
+            this.ngenButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ngenButton.UseVisualStyleBackColor = true;
-            this.ngenButton.Click += new System.EventHandler(this.ngenButton_Click);
             // 
             // createXmlButton
             // 
             this.createXmlButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.createXmlButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.createXmlButton.Image = global::SlimGen.Properties.Resources.create;
             this.createXmlButton.Location = new System.Drawing.Point(266, 3);
             this.createXmlButton.Name = "createXmlButton";
-            this.createXmlButton.Size = new System.Drawing.Size(258, 105);
+            this.createXmlButton.Size = new System.Drawing.Size(258, 106);
             this.createXmlButton.TabIndex = 3;
             this.createXmlButton.Text = "Create XML Configuration";
+            this.createXmlButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.createXmlButton.UseVisualStyleBackColor = true;
-            this.createXmlButton.Click += new System.EventHandler(this.createXmlButton_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(553, 379);
+            this.ClientSize = new System.Drawing.Size(553, 378);
             this.Controls.Add(this.groupBox);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "MainForm";
             this.Text = "SlimGen";
             this.groupBox.ResumeLayout(false);
