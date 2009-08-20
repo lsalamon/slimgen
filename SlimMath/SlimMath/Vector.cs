2b201eb96ec735bd54b6818d39e63aac65f67e9b
ryoohki <ryoohki@dc685512-7b07-11de-a624-ab7c8f4ec817>
2009-08-20 23:23:04 +0000

    Added base SlimMath

---
+++ SlimMath/SlimMath/Vector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SlimMath {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vector {
+        public float X, Y, Z, W;
+    }
+}
