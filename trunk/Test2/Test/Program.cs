c766ece20401600494e29dbb51c2f4d7ee040ef4
mike.popoloski <mike.popoloski@dc685512-7b07-11de-a624-ab7c8f4ec817>
2010-06-02 01:07:13 +0000

    Bah.

--- trunk/Test2/Test/Program.cs
+++ trunk/Test2/Test/Program.cs
@@ -15,6 +15,7 @@ namespace Test
 
             var method = new MethodReplacement(typeof(Vector4).GetMethod("Dot"), Platform.X64, InstructionSets.SSE41, new[] { File.ReadAllBytes("dot.obj") });
             injector.Replace(new[] { method });
+            Console.WriteLine(injector.Errors);
 
             float result = Vector4.Dot(Vector4.One, Vector4.UnitY);
             Console.WriteLine(result);
