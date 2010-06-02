55c214cd8d22d9e375a01ce4e1710dd65c876c81
mike.popoloski <mike.popoloski@dc685512-7b07-11de-a624-ab7c8f4ec817>
2010-06-02 01:01:10 +0000

    Added test project.

---
+++ trunk/Test2/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SlimMath;
+using SlimGen;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var injector = new Injector("debugger.exe");
+
+            var method = new MethodReplacement(typeof(Vector4).GetMethod("Dot"), Platform.X64, InstructionSets.SSE41, new[] { File.ReadAllBytes("dot.obj") });
+            injector.Replace(new[] { method });
+
+            float result = Vector4.Dot(Vector4.One, Vector4.UnitY);
+            Console.WriteLine(result);
+        }
+    }
+}
